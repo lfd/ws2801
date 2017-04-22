af79773f08d23a34f3913971e66488e04d8b5d65
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2017-04-23 00:44:18 +0200

    add license headers
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- driver/ws2801-kernel.c
+++ driver/ws2801-kernel.c
@@ -1,3 +1,15 @@
+/*
+ * ws2801 - WS2801 LED driver running in Linux userspace
+ *
+ * Copyright (c) - Ralf Ramsauer, 2017
+ *
+ * Authors:
+ *   Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
--- include.mk
+++ include.mk
@@ -1,3 +1,13 @@
+# ws2801 - WS2801 LED driver running in Linux userspace
+#
+# Copyright (c) - Ralf Ramsauer, 2017
+#
+# Authors:
+#   Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+#
+# This work is licensed under the terms of the GNU GPL, version 2.  See
+# the COPYING file in the top-level directory.
+
 CFLAGS += -Wall -Wstrict-prototypes -Wtype-limits \
 	  -Wmissing-declarations -Wmissing-prototypes
 CFLAGS += -O2
